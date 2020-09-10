e598571d9aed1c30f0e2682c3450e19454777269
Steve940 <smd940@gmail.com>
2020-09-10 15:18:51 -0400

    Adjusted println to display proper integer addition

--- compileTest.java
+++ compileTest.java
@@ -5,7 +5,7 @@ public class compileTest {
 		// TODO Auto-generated method stub
 		int x = 5;
 		int y = 2;
-		System.out.println("x + y = " + x + y);
+		System.out.println("x + y = " + (x+y));
 	}
 
 }
