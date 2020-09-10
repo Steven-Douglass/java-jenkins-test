8c723edd339c4c6240f22a348712e362f29ab489
Steve940 <smd940@gmail.com>
2020-09-10 15:15:08 -0400

    Added compileTest.java

---
+++ compileTest.java
@@ -0,0 +1,11 @@
+
+public class compileTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 2;
+		System.out.println("x + y = " + x + y);
+	}
+
+}
